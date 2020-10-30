043a7272926b2b1b60998e130dc6eaa8d76894a6
Shahriyar Rahman <58167217+shahriyarRahman@users.noreply.github.com>
2020-10-30 05:49:19 +0500

    shop page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,4 @@
 
-$('.owl-carousel').owlCarousel({
-    loop: false,
-    autoWidth: true,
-    margin: 150,
-    dots: true,
-    dotsEach: true,
-    responsiveClass: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 2
-        },
-        1000: {
-            items: 3
-        },
-        1400: {
-            items: 4
-        }
-    }
-})
-
 $(document).ready(function () {
     $('.toggle-botton').on('click', () => {
         $(".collapsable").addClass("show");
